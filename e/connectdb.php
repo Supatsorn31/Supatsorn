711dea64141c89bad1dcde5a996e25c59da0e7ce
Supatsorn31 <66010914038@msu.ac.th>
2026-01-28 16:02:52 +0700

    a

--- e/connectdb.php
+++ e/connectdb.php
@@ -1,7 +1,7 @@
 <?php
    $host = "localhost";
    $user = "root";
-   $pwd = "";
+   $pwd = "r660109";
    $db = "4038db";
    $conn = mysqli_connect($host,$user,$pwd,$db) or die ("เชื่อมต่อฐานข้อมูลไม่ได้");
    mysqli_query($conn, "SET NAMES utf8"); //เชื่อมต่อฐานข้อมูลดาต้าเบส
