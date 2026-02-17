bc682d696ca17c44c0bf2ca3595a768ba4282a11
Supatsorn <66010914038@msu.ac.th>
2026-02-17 19:46:49 +0700

    a

--- i/a.php
+++ i/a.php
@@ -38,7 +38,7 @@ if(isset($_POST['Submit'])){
      <tr>
          <td><?php echo $data['r_id']; ?></td>
          <td><?php echo $data['r_name']; ?></td>
-         <td width="80" align="center"><a href="delete_region.php?id=<?php echo $data['r_id']; ?>" onClick="return confirm('ยืนยันการลบ?');"><img src="images/delete.png" width="20"></a></td>
+         <td width="80" align="center"><a href="delete_region.php?id=<?php echo $data['r_id']; ?>" onClick="return confirm('ยืนยันการลบ?');"><img src="images/delete.jpg" width="20"></a></td>
      </tr>
  <?php } ?>
  </table>
